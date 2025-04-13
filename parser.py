10fdc186d0eba6def33829af24c6caedd7e6efdf
Maisonig <Israelyan-harry@mail.ru>
2025-04-13 08:53:48 +0300

    Added a function for creating a dataset

--- parser.py
+++ parser.py
@@ -1,3 +1,4 @@
+import os
 from pickle import UnpicklingError
 from typing import Optional
 
@@ -17,9 +18,9 @@ def load_xy_from_pt(path) -> dict:
 
     try:
         data = torch.load(path)
-        print("Файл загружен.")
+        print(f"Файл {path} загружен.")
     except UnpicklingError:
-        print("Не удалось загрузить данные. Неверно указан путь к файлу.")
+        print(f"Не удалось загрузить данные. Неверно указан путь к файлу {path}.")
         return
 
     if 'X' not in data or 'y' not in data:
@@ -67,16 +68,72 @@ def plot_series(data, sample_num: int = 155, start_idx: int = 0, num_indexes: in
 
 def create_dataset(datasets: list[dict],
                    sequence_length: int = 2000,
-                   train_num: int = 70,
-                   val_num: int = 20,
-                   test_num: int = 10):
+                   train_part: int = 0.7,
+                   val_part: int = 0.2):
+    """
+        Функция объединяет несколько датасетов в 1 общий и разделяет его на тренировочную, валидационную и тестовую выборку
+        согласно входным параметрам. Если число временных меток в датасете не делится нацело на требуемую длину секции,
+        то датасет обрезается и часть данных (последний временной ряд, который меньше длины секции) в обучении не
+        используется
 
+    :param datasets: Список со словарями датасетов
+    :param sequence_length: Длина секции, 1 сэмпла
+    :param train_part: Процент сэмплов для обучения
+    :param val_part: Процент сэмплов для валидации
+    :param test_part: Процент сэмплов для теста
+
+    :return:
+    """
+    xs, ys = None, None
     for dataset in datasets:
-        pass
+        x, y = torch.flatten(dataset['X'].t()), torch.flatten(dataset['y'].t())
+        sequences_num = len(x) // sequence_length
+        sequences_length = int(sequences_num * sequence_length)
+        x, y = x[:sequences_length], y[:sequences_length]
+        if xs is None and ys is None:
+            xs, ys = x, y
+        else:
+            xs = torch.cat([xs, x], dim=0)
+            ys = torch.cat([ys, y], dim=0)
+
+    print(f"Всего временных меток {len(xs)}")
+
+    sequences_num = len(xs) // sequence_length
+    xs = xs.reshape([sequence_length, sequences_num])
+    ys = ys.reshape([sequence_length, sequences_num])
+
+    train_num = int(sequences_num * train_part)
+    val_num = int(sequences_num * val_part)
+
+    print(f"При длине секции {sequence_length} временных меток:")
+    print(f"\tЧисло сэмплов обучающей выборки: {train_num}")
+    print(f"\tЧисло сэмплов валидационной выборки: {val_num}")
+    print(f"\tЧисло сэмплов тестовой выборки: {sequences_num - train_num - val_num}")
+
+    train_xs = xs[:, :train_num]
+    val_xs = xs[:, train_num:train_num + val_num]
+    test_xs = xs[:, train_num + val_num:]
+
+    train_ys = ys[:, :train_num]
+    val_ys = ys[:, train_num:train_num + val_num]
+    test_ys = ys[:, train_num + val_num:]
+
+    train_dataset = {'X': train_xs, 'y': train_ys}
+    val_dataset = {'X': val_xs, 'y': val_ys}
+    test_dataset = {'X': test_xs, 'y': test_ys}
+
+    return train_dataset, val_dataset, test_dataset
 
 
-data = load_xy_from_pt('tensor_dataset3.pt')
-a = torch.flatten(data['X'].t())
-b = torch.flatten(data['y'].t())
-c = {'X': a, 'y': b}
-plot_series(c, sample_num=0)
+datasets = []
+for path in os.listdir('./'):
+    if path.endswith('.pt'):
+        datasets.append(load_xy_from_pt(path))
+train_dataset, val_dataset, test_dataset = create_dataset(datasets)
+
+class SimpleNet(torch.nn.Module):
+    def __init__(self):
+        super(SimpleNet, self).__init__()
+
+model = SimpleNet()
+model.train()
